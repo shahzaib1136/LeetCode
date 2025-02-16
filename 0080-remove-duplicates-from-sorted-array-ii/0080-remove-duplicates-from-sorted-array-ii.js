5d2cee9e094f040046683a8cb86c05e275445ebc
shahzaib1136 <shahzaib1136@gmail.com>
2025-02-16 20:58:29 +0000

    Time: 75 ms (12.39%), Space: 53.1 MB (16.34%) - LeetHub

---
+++ 0080-remove-duplicates-from-sorted-array-ii/0080-remove-duplicates-from-sorted-array-ii.js
@@ -0,0 +1,16 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var removeDuplicates = function(nums) {
+    let k = 0;
+    
+    for(let i =0; i < nums.length; i++){
+        if(nums[i+2] !== nums[i]){
+            nums[k] = nums[i];
+            k++
+        }
+    }
+
+    return k;
+};
